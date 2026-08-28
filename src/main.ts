0b5d1e95fafb223fe4fa8b6f2b21db9bda9da8b7
Jaid <6216144+Jaid@users.noreply.github.com>
2026-08-28 22:12:43 +0100

    Improved performance
    Signed-off-by: Jaid <6216144+Jaid@users.noreply.github.com>

--- src/main.ts
+++ src/main.ts
@@ -30,39 +30,42 @@ export const makeBase64 = (input: Input, options: Options = {}) => {
   const symbols = getSymbols(options.symbols)
   const bytes = toBytes(input)
   const fullLength = bytes.length - bytes.length % 3
-  const output: Array<string> = []
-  const pad = () => {
-    if (!options.pad) {
-      return
-    }
-    const padString = options.pad === true ? '=' : options.pad
-    output.push(padString)
-  }
+  let output = ''
   for (let index = 0; index < fullLength; index += 3) {
     const byte0 = bytes[index]
     const byte1 = bytes[index + 1]
     const byte2 = bytes[index + 2]
-    output.push(symbols[byte0 >> 2])
-    output.push(symbols[(byte0 & 0x03) << 4 | byte1 >> 4])
-    output.push(symbols[(byte1 & 0x0F) << 2 | byte2 >> 6])
-    output.push(symbols[byte2 & 0x3F])
+    output += symbols[byte0 >> 2]
+    output += symbols[(byte0 & 0x03) << 4 | byte1 >> 4]
+    output += symbols[(byte1 & 0x0F) << 2 | byte2 >> 6]
+    output += symbols[byte2 & 0x3F]
   }
   const remainder = bytes.length - fullLength
+  if (!remainder) {
+    return output
+  }
+  const getPadString = () => {
+    if (!options.pad) {
+      return ''
+    }
+    if (options.pad === true) {
+      return '='
+    }
+    return options.pad
+  }
+  const pad = getPadString()
   if (remainder === 1) {
     const byte0 = bytes[fullLength]
-    output.push(symbols[byte0 >> 2])
-    output.push(symbols[(byte0 & 0x03) << 4])
-    pad()
-    pad()
-  } else if (remainder === 2) {
-    const byte0 = bytes[fullLength]
-    const byte1 = bytes[fullLength + 1]
-    output.push(symbols[byte0 >> 2])
-    output.push(symbols[(byte0 & 0x03) << 4 | byte1 >> 4])
-    output.push(symbols[(byte1 & 0x0F) << 2])
-    pad()
+    output += symbols[byte0 >> 2]
+    output += symbols[(byte0 & 0x03) << 4]
+    return output + pad + pad
   }
-  return output.join('')
+  const byte0 = bytes[fullLength]
+  const byte1 = bytes[fullLength + 1]
+  output += symbols[byte0 >> 2]
+  output += symbols[(byte0 & 0x03) << 4 | byte1 >> 4]
+  output += symbols[(byte1 & 0x0F) << 2]
+  return output + pad
 }
 
 export default makeBase64
